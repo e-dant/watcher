bd856eb49348900d5033193ff3946636a27f4eae
Will <edant.io@proton.me>
2022-10-16 18:27:10 -0400

    sinclude/watcher.hpp: single-header include

---
+++ sinclude/watcher.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+/*
+  @brief watcher/watcher
+
+  This is the public interface.
+  Include and use this file.
+*/
+
+#include <watcher/platform.hpp>
+#include <watcher/event.hpp>
+#include <watcher/watch.hpp>
