ae079af7fdda424cd8baee24b073454d84fdd2ed
Will <edant.io@proton.me>
2022-10-17 09:46:43 -0400

    hooks/pre-commit -> clang-format + amalgam

--- include/watcher/watcher.hpp
+++ include/watcher/watcher.hpp
@@ -7,6 +7,7 @@
   Include and use this file.
 */
 
+/* clang-format off */
 #include <watcher/platform.hpp>
 #include <watcher/event.hpp>
 #include <watcher/adapter/windows/watch.hpp>
@@ -15,3 +16,4 @@
 #include <watcher/adapter/linux/watch.hpp>
 #include <watcher/adapter/android/watch.hpp>
 #include <watcher/watch.hpp>
+/* clang-format on */
