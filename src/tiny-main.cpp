deb60b0beb9a49c9759e956286231a794e970d15
Will <edant.io@proton.me>
2022-10-17 21:54:38 -0400

    src/tiny-main: a few comments are fine + doc

--- src/tiny-main.cpp
+++ src/tiny-main.cpp
@@ -1,12 +1,15 @@
+/* tiny-main.cpp */
 #include <iostream>
-#include "../sinclude/watcher/watcher.hpp"
+#include "../sinclude/watcher/watcher.hpp" /* Point this to wherever yours is */
 int main(int argc, char** argv) {
-  using water::watcher::event::event, water::watcher::watch, std::cout,
-      std::endl;
+  using water::watcher::event::event, water::watcher::watch, std::cout, std::endl;
   cout << R"({"water.watcher.stream":{)" << endl;
-  const bool is_watch_ok = watch<16>(
+
+  /* This is the important line. */
+  const auto is_watch_ok = watch<16>(
       argc > 1 ? argv[1] : ".",
       [](const event& this_event) { cout << this_event << ',' << endl; });
+
   cout << '}' << endl << '}' << endl;
   return is_watch_ok;
 }
