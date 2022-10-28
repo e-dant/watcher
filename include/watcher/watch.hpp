4f4d56ddba19f9de762c9530241f20af522f55d3
Will <edant.io@proton.me>
2022-10-27 21:48:14 -0400

    watch: doc + rm die ovl

--- include/watcher/watch.hpp
+++ include/watcher/watch.hpp
@@ -6,87 +6,50 @@
 namespace water {
 namespace watcher {
 
-/*
-  @brief watcher/watch
+/* @brief watcher/watch
 
-  Implements `water::watcher::watch`.
+   @param path:
+     The root path to watch for filesystem events.
 
-  There are two things the user needs:
-    - The `watch` function
-    - The `die` function
-    - The `event` structure
+   @param living_cb (optional):
+     Something (such as a closure) to be called when events
+     occur in the path being watched.
 
-  That's it, and this is one of them.
+   This is an adaptor "switch" that chooses the ideal adaptor
+   for the host platform.
 
-  Happy hacking.
+   Every adapter monitors `path` for changes and invokes the
+   `callback` with an `event` object when they occur.
 
-  @param callback (optional):
-    Something (such as a function or closure) to be called
-    whenever events occur in the paths being watched.
+   There are two things the user needs:
+     - The `die` function
+     - The `watch` function
+     - The `event` structure
 
-  @param path:
-    The root path to watch for filesystem events.
+   That's it.
 
-  This is an adaptor "switch" that chooses the ideal adaptor
-  for the host platform.
-
-  Every adapter monitors `path` for changes and invoked the
-  `callback` with an `event` object when they occur.
-
-  The `event` object will contain the:
-    - Path -- Which is always relative.
-    - Path type -- one of:
-      - File
-      - Directory
-      - Symbolic Link
-      - Hard Link
-      - Unknown
-    - Event type -- one of:
-      - Create
-      - Modify
-      - Destroy
-      - OS-Specific Events
-      - Unknown
-    - Event time -- In nanoseconds since epoch
-*/
-
-static bool watch(const char* path, event::callback const& callback)
+   Happy hacking. */
+static bool watch(const char* path, event::callback const& living_cb)
 {
-  return detail::adapter::can_watch() ? detail::adapter::watch(path, callback)
+  return detail::adapter::can_watch() ? detail::adapter::watch(path, living_cb)
                                       : false;
 }
 
-static bool watch(const char* path, event::callback const& living_cb,
-                  event::callback const& dying_cb)
-{
-  using namespace water::watcher::detail;
+/* @brief watcher/die
 
-  auto ok = adapter::can_watch() ? adapter::watch(path, living_cb) : false;
-
-  adapter::die(dying_cb);
-
-  return ok;
-}
-
-/*
-  @brief watcher/die
-
-  Stops the `watch`.
-  Destroys itself.
-*/
+   Stops the `watch`.
+   Destroys itself. */
 static bool die()
 {
   using whatever = const event::event&;
   return detail::adapter::die([](whatever) -> void {});
 }
 
-/*
-  @brief watcher/die
+/* @brief watcher/die
 
-  Stops the `watch`.
-  Calls `callback`,
-  then destroys itself.
-*/
+   Stops the `watch`.
+   Calls `callback`,
+   then destroys itself. */
 static bool die(event::callback const& callback)
 {
   return detail::adapter::die(callback);
