2463e5248f83e0ca5e61b1c51919efa1af61c075
Will <edant.io@proton.me>
2023-01-01 21:54:56 -0500

    adapter/windows: tlc

--- include/watcher/adapter/windows/watch.hpp
+++ include/watcher/adapter/windows/watch.hpp
@@ -11,10 +11,15 @@
 
 #include <stdio.h>
 #include <windows.h>
+/* milliseconds */
 #include <chrono>
-#include <iostream>
+/* string */
 #include <string>
+/* path */
+#include <filesystem>
+/* this_thread::sleep_for */
 #include <thread>
+/* vector */
 #include <vector>
 
 namespace wtr {
@@ -22,6 +27,14 @@ namespace watcher {
 namespace detail {
 namespace adapter {
 namespace {
+
+inline constexpr auto delay_ms = std::chrono::milliseconds(16);
+
+/* I think the default page size in Windows is 64kb,
+   so 65536 might also work well.
+*/
+inline constexpr auto event_buf_len_max = 8192;
+
 namespace util {
 
 inline std::string wstring_to_string(std::wstring const& in) noexcept
@@ -43,6 +56,7 @@ inline std::string wstring_to_string(std::wstring const& in) noexcept
   }
 }
 
+/*
 inline std::wstring string_to_wstring(std::string const& in) noexcept
 {
   size_t in_len = MultiByteToWideChar(CP_UTF8, 0, in.data(),
@@ -68,199 +82,195 @@ inline std::wstring cstring_to_wstring(char const* in) noexcept
   mbstowcs_s(nullptr, out.data(), in_len + 1, in, in_len);
   return std::wstring(out.data());
 }
+*/
 
 } /* namespace util */
 
-/* The watch event overlap holds an event buffer */
-struct watch_event_overlap
+/* Hold resources necessary to recieve and send filesystem events. */
+class watch_event_accumulator
 {
-  OVERLAPPED o;
+ private:
+  bool valid{false};
 
-  FILE_NOTIFY_INFORMATION* buffer;
-
-  unsigned buffersize;
-};
-
-/* The watch object holds a watch event overlap */
-struct watch_object
-{
+ public:
   wtr::watcher::event::callback const& callback;
 
-  HANDLE event_completion_token;
+  HANDLE event_completion_token{nullptr};
 
-  HANDLE hdirectory;
+  HANDLE event_thread_handle{new HANDLE{}};
 
-  HANDLE hthreads;
+  HANDLE event_token{CreateEventW(nullptr, true, false, nullptr)};
 
-  HANDLE event_token;
+  OVERLAPPED event_overlap{};
 
-  watch_event_overlap* wolap;
+  FILE_NOTIFY_INFORMATION event_buf[event_buf_len_max];
 
-  int working;
+  unsigned event_buf_len_ready{0};
 
-  wchar_t directoryname[256];
+  bool working{false};
+
+  wchar_t path_name[256]{L""};
+
+  HANDLE path_handle{nullptr};
+
+  watch_event_accumulator(std::filesystem::path const& path,
+                          event::callback const& callback) noexcept
+      : callback{callback}
+  {
+    memcpy(path_name, path.c_str(), path.string().size());
+
+    path_handle = CreateFileW(
+        path.c_str(), FILE_LIST_DIRECTORY,
+        FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, nullptr,
+        OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED,
+        nullptr);
+
+    if (path_handle) {
+      event_completion_token
+          = CreateIoCompletionPort(INVALID_HANDLE_VALUE, nullptr, 0, 0);
+
+      if (event_completion_token) {
+        CreateIoCompletionPort(path_handle, event_completion_token,
+                               (ULONG_PTR)path_handle, 1);
+        valid = true;
+      }
+    }
+  }
+
+  ~watch_event_accumulator() noexcept
+  {
+    CloseHandle(event_token);
+    delete event_thread_handle;
+  }
+
+  bool is_valid() const noexcept
+  {
+    return valid && event_buf != nullptr;
+  }
+
+  bool has_unsent() const noexcept
+  {
+    return event_buf_len_ready != 0;
+  }
 };
 
-FILE_NOTIFY_INFORMATION* do_event_recv(watch_event_overlap* wolap,
-                                       FILE_NOTIFY_INFORMATION* buffer,
-                                       unsigned* bufferlength,
-                                       unsigned buffersize, HANDLE hdirectory,
-                                       event::callback const& callback) noexcept
+inline FILE_NOTIFY_INFORMATION* do_event_recv(
+    watch_event_accumulator* w) noexcept
 {
   using namespace wtr::watcher;
 
-  *bufferlength = 0;
+  FILE_NOTIFY_INFORMATION* buf = w->event_buf;
+
+  w->event_buf_len_ready = 0;
   DWORD bytes_returned = 0;
   OVERLAPPED* po;
-  memset(&wolap->o, 0, sizeof(OVERLAPPED));
-  po = &wolap->o;
+  memset(&w->event_overlap, 0, sizeof(OVERLAPPED));
+  po = &w->event_overlap;
 
   if (ReadDirectoryChangesW(
-          hdirectory, buffer, buffersize, true,
+          w->path_handle, buf, event_buf_len_max, true,
           FILE_NOTIFY_CHANGE_SECURITY | FILE_NOTIFY_CHANGE_CREATION
               | FILE_NOTIFY_CHANGE_LAST_ACCESS | FILE_NOTIFY_CHANGE_LAST_WRITE
               | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_ATTRIBUTES
               | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_FILE_NAME,
           &bytes_returned, po, nullptr))
   {
-    *bufferlength = bytes_returned > 0 ? bytes_returned : 0;
-    buffer = bytes_returned > 0 ? buffer : nullptr;
+    w->event_buf_len_ready = bytes_returned > 0 ? bytes_returned : 0;
+    buf = bytes_returned > 0 ? buf : nullptr;
   } else {
     switch (GetLastError()) {
       case ERROR_IO_PENDING:
-        *bufferlength = 0;
-        buffer = nullptr;
-        callback(event::event{"e/watcher/rdc/io_pending", event::what::other,
-                              event::kind::watcher});
+        w->event_buf_len_ready = 0;
+        buf = nullptr;
+        w->callback(event::event{"e/watcher/rdc/io_pending", event::what::other,
+                                 event::kind::watcher});
         break;
       default:
-        callback(event::event{"e/watcher/rdc", event::what::other,
-                              event::kind::watcher});
+        w->callback(event::event{"e/watcher/rdc", event::what::other,
+                                 event::kind::watcher});
         break;
     }
   }
 
-  return buffer;
+  return buf;
 }
 
-unsigned do_event_send(FILE_NOTIFY_INFORMATION* pfni,
-                       unsigned const bufferlength, wchar_t const* dirname_wc,
-                       event::callback const& callback) noexcept
+inline void do_event_send(watch_event_accumulator* w) noexcept
 {
-  unsigned result = 0;
+  FILE_NOTIFY_INFORMATION* buf = w->event_buf;
 
-  while (pfni + sizeof(FILE_NOTIFY_INFORMATION) <= pfni + bufferlength) {
-    if (pfni->FileNameLength % 2 == 0) {
+  while (buf + sizeof(FILE_NOTIFY_INFORMATION) <= buf + w->event_buf_len_ready)
+  {
+    if (buf->FileNameLength % 2 == 0) {
       auto path = util::wstring_to_string(
-          std::wstring{dirname_wc, wcslen(dirname_wc)} + std::wstring{L"\\"}
-          + std::wstring{pfni->FileName, pfni->FileNameLength / 2});
+          std::wstring{w->path_name, wcslen(w->path_name)} + std::wstring{L"\\"}
+          + std::wstring{buf->FileName, buf->FileNameLength / 2});
 
-      switch (pfni->Action) {
+      switch (buf->Action) {
         case FILE_ACTION_MODIFIED:
-          callback({path, event::what::modify, event::kind::file});
+          w->callback({path, event::what::modify, event::kind::file});
           break;
         case FILE_ACTION_ADDED:
-          callback({path, event::what::create, event::kind::file});
+          w->callback({path, event::what::create, event::kind::file});
           break;
         case FILE_ACTION_REMOVED:
-          callback({path, event::what::destroy, event::kind::file});
+          w->callback({path, event::what::destroy, event::kind::file});
           break;
         case FILE_ACTION_RENAMED_OLD_NAME:
-          callback({path, event::what::rename, event::kind::file});
+          w->callback({path, event::what::rename, event::kind::file});
           break;
         case FILE_ACTION_RENAMED_NEW_NAME:
-          callback({path, event::what::rename, event::kind::file});
+          w->callback({path, event::what::rename, event::kind::file});
           break;
         default: break;
       }
-      result++;
 
-      if (pfni->NextEntryOffset == 0)
+      if (buf->NextEntryOffset == 0)
         break;
       else
-        pfni = (FILE_NOTIFY_INFORMATION*)((uint8_t*)pfni
-                                          + pfni->NextEntryOffset);
+        buf = (FILE_NOTIFY_INFORMATION*)((uint8_t*)buf + buf->NextEntryOffset);
     }
   }
-
-  return result;
 }
 
-inline bool do_scan_work_async(watch_object* wobj, wchar_t const* directoryname,
-                               size_t dname_len,
-                               event::callback const& callback) noexcept
+inline bool do_scan_work_async(watch_event_accumulator* w,
+                               std::filesystem::path const& path) noexcept
 {
-  static constexpr unsigned buffersize = 65536;
+  if (w->is_valid()) {
+    w->working = true;
 
-  wobj->event_token = CreateEventW(nullptr, true, false, nullptr);
+    do_event_recv(w);
 
-  if (wobj->event_token) {
-    wobj->hthreads = (HANDLE*)HeapAlloc(GetProcessHeap(), 0, sizeof(HANDLE));
-    wobj->wolap = (watch_event_overlap*)HeapAlloc(GetProcessHeap(), 0,
-                                                  sizeof(watch_event_overlap));
-    wobj->working = true;
-    wobj->event_completion_token
-        = CreateIoCompletionPort(INVALID_HANDLE_VALUE, nullptr, 0, 0);
-    wcscpy_s(wobj->directoryname + 1, dname_len, directoryname);
-    wobj->directoryname[0] = static_cast<wchar_t>(wcslen(directoryname));
-    wobj->hdirectory = CreateFileW(
-        directoryname, FILE_LIST_DIRECTORY,
-        FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, nullptr,
-        OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED,
-        nullptr);
+    while (w->is_valid() && w->has_unsent()) {
+      do_event_send(w);
+    }
 
-    CreateIoCompletionPort(wobj->hdirectory, wobj->event_completion_token,
-                           (ULONG_PTR)wobj->hdirectory, 1);
-
-    FILE_NOTIFY_INFORMATION* buffer;
-    unsigned bufferlength;
-
-    wobj->wolap->buffersize = buffersize;
-    wobj->wolap->buffer = (FILE_NOTIFY_INFORMATION*)HeapAlloc(
-        GetProcessHeap(), 0, wobj->wolap->buffersize);
-    wobj->hthreads = nullptr;
-    buffer = wobj->wolap->buffer;
-    buffer = do_event_recv(wobj->wolap, buffer, &bufferlength, buffersize,
-                           wobj->hdirectory, callback);
-
-    while (buffer != nullptr && bufferlength != 0)
-      do_event_send(buffer, bufferlength, wobj->directoryname, callback);
-
-    ResetEvent(wobj->event_token);
-    wobj->hthreads = CreateThread(
+    ResetEvent(w->event_token);
+    w->event_thread_handle = CreateThread(
         nullptr, 0,
-        [](void* parameter) -> DWORD {
-          struct watch_object* wobj = (struct watch_object*)parameter;
+        [](void* event_thread_parameter) -> DWORD {
+          class watch_event_accumulator* w
+              = (class watch_event_accumulator*)event_thread_parameter;
 
-          ULONG_PTR completionkey;
-          DWORD byte_ready_count;
-
-          SetEvent(wobj->event_token);
+          SetEvent(w->event_token);
 
           SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_IDLE);
 
-          while (wobj->working) {
-            OVERLAPPED* po;
+          while (w->working) {
+            ULONG_PTR completion_key{};
+            DWORD byte_ready_count{};
+            OVERLAPPED* po{};
 
-            GetQueuedCompletionStatus(wobj->event_completion_token,
-                                      &byte_ready_count, &completionkey, &po,
+            GetQueuedCompletionStatus(w->event_completion_token,
+                                      &byte_ready_count, &completion_key, &po,
                                       INFINITE);
             if (po) {
-              watch_event_overlap* wolap
-                  = (watch_event_overlap*)CONTAINING_RECORD(
-                      po, watch_event_overlap, o);
-
               if (byte_ready_count) {
-                auto* buffer = (FILE_NOTIFY_INFORMATION*)wolap->buffer;
-                unsigned bufferlength = byte_ready_count;
+                w->event_buf_len_ready = byte_ready_count;
 
-                while (wobj->working && buffer && bufferlength) {
-                  do_event_send(buffer, bufferlength, wobj->directoryname,
-                                wobj->callback);
-
-                  buffer = do_event_recv(wolap, buffer, &bufferlength,
-                                         wolap->buffersize, wobj->hdirectory,
-                                         wobj->callback);
+                while (w->has_unsent()) {
+                  do_event_send(w);
+                  do_event_recv(w);
                 }
               }
             }
@@ -268,10 +278,9 @@ inline bool do_scan_work_async(watch_object* wobj, wchar_t const* directoryname,
 
           return 0;
         },
-        (void*)wobj, 0, nullptr);
-    if (wobj->hthreads) WaitForSingleObject(wobj->event_token, INFINITE);
+        (void*)w, 0, nullptr);
+    if (w->event_thread_handle) WaitForSingleObject(w->event_token, INFINITE);
   }
-  CloseHandle(wobj->event_token);
 
   return 0;
 }
@@ -283,48 +292,18 @@ inline bool do_scan_work_async(watch_object* wobj, wchar_t const* directoryname,
    return when dead
    true if no errors */
 
-inline bool watch(std::wstring const& path, event::callback const& callback,
+inline bool watch(std::filesystem::path const& path,
+                  event::callback const& callback,
                   auto const& is_living) noexcept
 {
   using std::this_thread::sleep_for, std::chrono::milliseconds;
 
-  static constexpr auto delay_ms = 16;
-
-  /* @todo We need to find a better way of dealing with wide strings.
-     This shouldn't affect us too much in the perf department because
-     this is not the hot path. I wouldn't be surprised if the character
-     strings lose information after all this back-and-forth. */
-  auto path_str = util::wstring_to_string(path);
-
-  /* MSVC rejects the `.directoryname` field with this message:
-     `C99 designator 'directoryname' outside aggregate initializer`.
-     So, we're not using aggregate initializers now. */
-  do_scan_work_async(new watch_object{callback, nullptr, nullptr, nullptr,
-                                      nullptr, nullptr, 0, L""},
-                     path.c_str(), path.size() + 1, callback);
+  do_scan_work_async(new watch_event_accumulator{path, callback}, path);
 
   while (is_living())
-    if constexpr (delay_ms > 0) sleep_for(milliseconds(delay_ms));
+    if constexpr (delay_ms > milliseconds(0)) sleep_for(delay_ms);
 
   return true;
-}
-
-inline bool watch(wchar_t const* path, event::callback const& callback,
-                  auto const& is_living) noexcept
-{
-  return watch(std::wstring{path, wcslen(path)}, callback, is_living);
-}
-
-inline bool watch(char const* path, event::callback const& callback,
-                  auto const& is_living) noexcept
-{
-  return watch(util::cstring_to_wstring(path), callback, is_living);
-}
-
-inline bool watch(std::string const& path, event::callback const& callback,
-                  auto const& is_living) noexcept
-{
-  return watch(util::string_to_wstring(path), callback, is_living);
 }
 
 } /* namespace adapter */
