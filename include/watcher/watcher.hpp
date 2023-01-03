4465240089fbf9b72b3e27718eb4b3403421f82d
Will <edant.io@proton.me>
2023-01-02 21:46:05 -0500

    watcher/watcher: incl order

--- include/watcher/watcher.hpp
+++ include/watcher/watcher.hpp
@@ -10,11 +10,11 @@
 /* clang-format off */
 #include <watcher/platform.hpp>
 #include <watcher/event.hpp>
-#include <watcher/adapter/adapter.hpp>
 #include <watcher/adapter/windows/watch.hpp>
 #include <watcher/adapter/darwin/watch.hpp>
 #include <watcher/adapter/linux/watch.hpp>
 #include <watcher/adapter/android/watch.hpp>
 #include <watcher/adapter/warthog/watch.hpp>
+#include <watcher/adapter/adapter.hpp>
 #include <watcher/watch.hpp>
 /* clang-format on */
