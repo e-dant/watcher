bb65f84d056b355b39f9888441e296298565e349
Will <edant.io@proton.me>
2023-01-02 22:32:42 -0500

    tool/format

--- include/watcher/adapter/adapter.hpp
+++ include/watcher/adapter/adapter.hpp
@@ -106,8 +106,7 @@ inline bool adapter(std::filesystem::path const& path,
        - Tell a watcher to die
      There may be more messages in the future. */
   if (msg)
-    return live()
-           && watch(path, callback, is_living);
+    return live() && watch(path, callback, is_living);
 
   else
     return die();
