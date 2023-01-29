e9525203f141606e95c82359a46d614a52021e21
Will <edant.io@proton.me>
2023-01-28 21:25:39 -0500

    update/snitch: 1.0.0

--- build/in/cmake/wtr.test_watcher.cmake
+++ build/in/cmake/wtr.test_watcher.cmake
@@ -4,9 +4,10 @@ include(FetchContent)
 FetchContent_Declare(
   snitch
   GIT_REPOSITORY https://github.com/cschreib/snitch.git
-  GIT_TAG        8165d6c85353f9c302ce05f1c1c47dcfdc6aeb2c # Saturday, January 7th, 2023 @ main
+  GIT_TAG        ea200a0830394f8e0ef732064f0935a77c003bd6 # Friday, January 20th, 2023 @ v1.0.0
+  # GIT_TAG        8165d6c85353f9c302ce05f1c1c47dcfdc6aeb2c # Saturday, January 7th, 2023 @ main
+  # GIT_TAG        f313bccafe98aaef617af3bf457d091d8d50cdcd # Tuesday, December 18th, 2022 @ v0.1.3
   # GIT_TAG        c0b6ac4efe4019e4846e8967fe21de864b0cc1ed # Friday, December 2nd, 2022 @ main
-  # GIT_TAG        f313bccafe98aaef617af3bf457d091d8d50cdcd # version 0.1.3
 )
 FetchContent_MakeAvailable(snitch)
 
