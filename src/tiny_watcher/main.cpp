174130ff1993163e1fe0400dc9ef2b280083ea78
Will <edant.io@proton.me>
2023-02-12 13:52:22 -0500

    tool/format

--- src/tiny_watcher/main.cpp
+++ src/tiny_watcher/main.cpp
@@ -1,17 +1,21 @@
+#include "../../include/wtr/watcher.hpp"  // Point this to wherever yours is
 #include <iostream>
-#include "../../include/wtr/watcher.hpp" // Point this to wherever yours is
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
   // This is the entire API.
   using namespace wtr::watcher;
-  auto watcher = watch(argc > 1 ? argv[1] : ".", [](event::event const& ev) {
-    std::cout << ev << "," << std::endl;
-    // Or, manually:
-    // std::cout << "event at path: " << ev.where << "\n"
-    //           << "kind of path: " << ev.kind << "\n"
-    //           << "event type: " << ev.what << "\n"
-    //           << "nanoseconds since epoch: " << ev.when << std::endl;
-  });
+  auto watcher = watch(argc > 1 ? argv[1] : ".",
+                       [](event::event const& ev)
+                       {
+                         std::cout << ev << "," << std::endl;
+                         // Or, manually:
+                         // std::cout << "event at path: " << ev.where << "\n"
+                         //           << "kind of path: " << ev.kind << "\n"
+                         //           << "event type: " << ev.what << "\n"
+                         //           << "nanoseconds since epoch: " << ev.when
+                         //           << std::endl;
+                       });
   // Close whenever you're done.
   // watcher.close();
 }
