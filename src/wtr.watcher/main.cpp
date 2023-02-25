a38ef1b6ca8ade119001f642fd4884a33c4b95ac
Will <edant.io@proton.me>
2023-02-24 22:26:53 -0500

    wtr.watcher: simplify

--- src/wtr.watcher/main.cpp
+++ src/wtr.watcher/main.cpp
@@ -1,149 +1,182 @@
-/* std::chrono::<units> */
+/*  std::chrono::* */
 #include <chrono>
-/* std::strcmp */
+/*  std::strcmp */
 #include <cstring>
-/* std::filesystem::current_path
-   std::filesystem::absolute
-   std::filesystem::exists
-   std::filesystem::path */
+/*  std::filesystem::
+      current_path
+      absolute
+      exists
+      path */
 #include <filesystem>
-/* std::this_thread::sleep_for */
-#include <thread>
-/* std::cout
-   std::endl */
+/*  std::cout
+    std::endl */
 #include <iostream>
-/* std::stoull */
+/*  std::stoull */
 #include <string>
-/* std::make_tuple */
+/*  std::make_tuple */
 #include <tuple>
-/* wtr::watcher::event::event
-   wtr::watcher::event::what
-   wtr::watcher::event::kind
-   wtr::watcher::watch
-   wtr::watcher::die */
+/*  std::this_thread::sleep_for */
+#include <thread>
+/*  wtr::watcher::
+      watch
+      event::
+        event
+        what
+        kind */
 #include <wtr/watcher.hpp>
 
-/* Watch a path for some time.
-   Or watch a path forever.
-   Show what happens. */
-int main(int argc, char** argv)
+using namespace wtr::watcher;
+using namespace std::chrono;
+using std::function;
+namespace fs = std::filesystem;
+
+auto watch_forever_or_expire(
+  std::optional<std::chrono::nanoseconds const> const& alive_for)
 {
-  using namespace wtr::watcher;
-  using namespace std::chrono_literals;
 
-  /* Lift the user's choices from the command line.
-     The options may be:
-     1. Path to watch (optional)
-     2. Time until death (optional)
-     3. Time unit (optional, defaults to milliseconds)
+  /*  Watch for some time, `alive_for`. */
+  auto expire = std::function<bool(std::filesystem::path const&,
+                                   wtr::event::callback const&)>{
+    [alive_for](std::filesystem::path const& path,
+                wtr::event::callback const& callback)
+    {
+      auto const then = system_clock::now();
+      std::cout << R"({"wtr":{"watcher":{"stream":{)" << std::endl;
 
-     If the path to watch is unspecified,
-     we use the user's current directory.
+      auto const die = wtr::watch(path, callback);
 
-     If we aren't told when to die,
-     we never do. */
-  auto const lift_options = [](auto const& argc, auto const& argv)
+      std::this_thread::sleep_for(alive_for.value());
+
+      auto const dead = die();
+
+      std::cout
+        << "}"
+        << "\n,\"milliseconds\":"
+        << duration_cast<milliseconds>(system_clock::now() - then).count()
+        << "\n,\"dead\":" << (dead ? "true" : "false") << "\n}}}" << std::endl;
+
+      return dead;
+    }};
+
+  /*  Watch forever. */
+  auto forever = std::function<bool(std::filesystem::path const&,
+                                    wtr::event::callback const&)>{
+    [](std::filesystem::path const& path, wtr::event::callback const& callback)
+    {
+      auto _{watch(path, callback)};
+      return true;
+    }};
+
+  if (alive_for.has_value())
+    return expire;
+
+  else
+    return forever;
+};
+
+auto from_cmdline(int const argc, char const** const argv)
+{
+  auto argis = [&](auto const i, char const* a)
+  { return argc > i ? std::strcmp(a, argv[i]) == 0 : false; };
+
+  auto given_or_current_path = [](int const argc, char const** const argv)
   {
-    using namespace std::chrono;
-
-    auto const path = [&]
-    {
-      namespace fs = std::filesystem;
-      auto const maybe_given_path = fs::path(argc > 1 ? argv[1] : ".");
-      return fs::exists(maybe_given_path) ? fs::absolute(maybe_given_path)
-                                          : fs::current_path();
-    };
-
-    auto const unit = [&](auto const& f)
-    {
-      auto const argis = [&](char const* a)
-      { return argc > 2 ? std::strcmp(a, argv[2]) == 0 : false; };
-      return argis("-nanoseconds") || argis("-ns")   ? nanoseconds(f())
-           : argis("-microseconds") || argis("-mcs") ? microseconds(f())
-           : argis("-milliseconds") || argis("-ms")  ? milliseconds(f())
-           : argis("-seconds") || argis("-s")        ? seconds(f())
-           : argis("-minutes") || argis("-m")        ? minutes(f())
-           : argis("-hours") || argis("-h")          ? hours(f())
-           : argis("-days") || argis("-d")           ? 24 * hours(f())
-           : argis("-weeks") || argis("-w")          ? 168 * hours(f())
-           : argis("-months") || argis("-mts")       ? 730 * hours(f())
-           : argis("-years") || argis("-y")          ? 8760 * hours(f())
-                                                     : milliseconds(f());
-    };
-
-    auto const help = [&]() -> std::function<bool()>
-    {
-      auto const argcmp = [&](auto const i, char const* a)
-      { return argc > i ? std::strcmp(a, argv[i]) == 0 : false; };
-      for (auto i = 0; i < argc; i++)
-        if (argcmp(i, "-h") || argcmp(i, "--help"))
-          return []
-          {
-            std::cout << "wtr.watcher [ path = . [ time = 0 [ unit = ms ] ] ]"
-                      << std::endl;
-            return true;
-          };
-      return [] { return false; };
-    };
-
-    auto const time = [&] { return argc > 3 ? std::stoull(argv[3]) : 0; };
-
-    return std::make_tuple(path(), unit(time), help());
+    return fs::absolute(
+      [&]
+      {
+        auto p = fs::path(argc > 1 ? argv[1] : ".");
+        return fs::exists(p) ? p : fs::current_path();
+      }());
   };
 
-  /* Show what happens.
-     Use the event's stream operator, formatting as json.
-     Append a comma until the last event: Watcher dying.
-     Flush stdout, via `endl`, for piping.
-     For manual parsing, see the file watcher/event.hpp. */
-  auto const stream_json = [](auto const& ev)
+  auto maybe_filter_callback = []
   {
-    using event::kind, event::what;
+    /*  Show what happens.
+        Use the event's stream operator, formatting as json.
+        Append a comma until the last event: Watcher dying.
+        Flush stdout, via `endl`, for piping.
+        For manual parsing, see the file watcher/event.hpp. */
+    wtr::event::callback show_json = [](wtr::event::event const& ev)
+    {
+      using wtr::watcher::event::kind, wtr::watcher::event::what;
 
-    auto const maybe_comma =
-      ev.kind == kind::watcher && ev.what == what::destroy ? "" : ",";
+      auto comma_or_nothing =
+        ev.kind == kind::watcher && ev.what == what::destroy ? "" : ",";
 
-    std::cout << ev << maybe_comma << std::endl;
+      std::cout << ev << comma_or_nothing << std::endl;
+    };
+
+    return show_json;
   };
 
-  auto const watch_expire =
-    [](auto const& path, auto const& callback, auto const& alive_for) -> bool
+  auto maybe_time = [&](int const argc, char const** const argv)
   {
-    using namespace std::chrono;
-    auto const then = system_clock::now();
-    std::cout << R"({"wtr":{"watcher":{"stream":{)" << std::endl;
+    auto given_or_zero_time = [&]()
+    {
+      auto t = [&] { return argc > 2 ? std::stoull(argv[2]) : 0; }();
 
-    /* This is the heart of the function.
-       Watching, concurrently. */
-    auto watcher = watch(path, callback);
+      auto targis = [&](char const* a) { return argis(3, a); };
 
-    /* Until our time is up. */
-    std::this_thread::sleep_for(alive_for);
-
-    /* Then dying. */
-    auto const dead = watcher.close();
-
-    std::cout << "}"
-              << "\n,\"milliseconds\":"
-              << duration_cast<milliseconds>(system_clock::now() - then).count()
-              << "\n,\"dead\":" << (dead ? "true" : "false") << "\n}}}"
-              << std::endl;
-
-    return dead;
+      // clang-format off
+      return targis("nanoseconds")  || targis("ns")  ? nanoseconds(t)
+           : targis("microseconds") || targis("us")  ? microseconds(t)
+           : targis("milliseconds") || targis("ms")  ? milliseconds(t)
+           : targis("seconds")      || targis("s")   ? seconds(t)
+           : targis("minutes")      || targis("m")   ? minutes(t)
+           : targis("hours")        || targis("h")   ? hours(t)
+           : targis("days")         || targis("d")   ? 24   * hours(t)
+           : targis("weeks")        || targis("w")   ? 168  * hours(t)
+           : targis("months")       || targis("mts") ? 730  * hours(t)
+           : targis("years")        || targis("y")   ? 8760 * hours(t)
+                                                     : milliseconds(t);
+      // clang-format on
+    }();
+    return given_or_zero_time > 0ns ? std::make_optional(given_or_zero_time)
+                                    : std::nullopt;
   };
 
-  auto const [path, alive_for, help] = lift_options(argc, argv);
+  auto maybe_help = [&]() -> std::optional<std::function<int()>>
+  {
+    auto help = []
+    {
+      std::cout << "wtr.watcher"
+                   " [ -h | --help ]"
+                   " [ path = . [ time = 0 [ unit = ms ] ] ]\n";
+      return 0;
+    };
 
-  /* Might show help,
-     Watch for some time,
-     Or run forever. */
-  return help()
+    if (argis(1, "-h") || argis(1, "--help"))
+      return help;
+    else
+      return std::nullopt;
+  };
 
-         ? 0
+  return std::make_tuple(given_or_current_path(argc, argv),
+                         maybe_filter_callback(),
+                         maybe_time(argc, argv),
+                         maybe_help());
+};
 
-         : alive_for > 0ns
+/*  wtr.watcher
+      [ -h | --help ]
+      [ -p = . [ -t = 0 [ unit = ms ] ] ]
 
-             ? ! watch_expire(path, stream_json, alive_for)
+      @todo
+      [ -what < all rename modify create destroy owner other > = all ]
+      [ -kind < all dir file hard_link sym_link watcher other > = all ]
 
-             : ! watch(path, stream_json).close();
-}
+    Watch a path for some time.
+    Or watch a path forever.
+    Show what happens, or show help. */
+int main(int const argc, char const** const argv)
+{
+  auto [path, callback, time, help] = from_cmdline(argc, argv);
+
+  if (help.has_value())
+    return help.value()();
+
+  else
+    return watch_forever_or_expire(time)(path, callback);
+
+  return 0;
+};
