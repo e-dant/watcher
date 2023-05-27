29813083870059e27d1397419673715874624b4f
Will <edant.io@proton.me>
2023-05-27 16:01:58 -0400

    tool/hone: amalgam

--- include/wtr/watcher.hpp
+++ include/wtr/watcher.hpp
@@ -474,8 +474,9 @@ do_event_send(watch_event_proxy& w,
         auto where =
           w.path / std::wstring{buf->FileName, buf->FileNameLength / 2};
 
-        auto what = [&buf]() noexcept -> enum event::what {
-          switch (buf->Action){
+        auto what = [&buf]() noexcept
+        {
+          switch (buf->Action) {
             case FILE_ACTION_MODIFIED : return event::what::modify;
             case FILE_ACTION_ADDED : return event::what::create;
             case FILE_ACTION_REMOVED : return event::what::destroy;
@@ -483,37 +484,36 @@ do_event_send(watch_event_proxy& w,
             case FILE_ACTION_RENAMED_NEW_NAME : return event::what::rename;
             default : return event::what::other;
           }
+        }();
+
+        auto kind = [&where]()
+        {
+          try {
+            return std::filesystem::is_directory(where) ? event::kind::dir
+                                                        : event::kind::file;
+          } catch (...) {
+            return event::kind::other;
+          }
+        }();
+
+        callback({where, what, kind});
+
+        if (buf->NextEntryOffset == 0)
+          break;
+        else
+          buf =
+            (FILE_NOTIFY_INFORMATION*)((uint8_t*)buf + buf->NextEntryOffset);
       }
-      ();
-
-      auto kind = [&where]()
-      {
-        try {
-          return std::filesystem::is_directory(where) ? event::kind::dir
-                                                      : event::kind::file;
-        } catch (...) {
-          return event::kind::other;
-        }
-      }();
-
-      callback({where, what, kind});
-
-      if (buf->NextEntryOffset == 0)
-        break;
-      else
-        buf = (FILE_NOTIFY_INFORMATION*)((uint8_t*)buf + buf->NextEntryOffset);
     }
+    return true;
   }
-  return true;
-}
 
-else
-{
-  return false;
-}
+  else {
+    return false;
+  }
 }  // namespace
 
-}  // namespace adapter
+}  // namespace
 
 /* while living
    watch for events
@@ -565,9 +565,9 @@ inline bool watch(std::filesystem::path const& path,
   }
 }
 
+}  // namespace adapter
 }  // namespace watcher
 }  // namespace wtr
-}  // namespace detail
 } /* namespace detail */
 
 #endif /* defined(WATER_WATCHER_PLATFORM_WINDOWS_ANY) */
@@ -2397,12 +2397,11 @@ inline auto open(std::filesystem::path const& path,
                  ::wtr::watcher::event::callback const& callback) noexcept
   -> future::shared
 {
-  using evw = enum ::wtr::watcher::event::what;
-  using evk = enum ::wtr::watcher::event::kind;
-
   auto fut = std::make_shared<future>();
 
-  callback({"s/self/live@" + path.string(), evw::create, evk::watcher});
+  callback({"s/self/live@" + path.string(),
+            ::wtr::watcher::event::what::create,
+            ::wtr::watcher::event::kind::watcher});
 
   fut->work = std::async(std::launch::async,
                          [path, callback, fut]() noexcept -> bool
