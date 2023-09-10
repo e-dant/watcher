a516d7598d75364479338e96b02f1e1b4e657d90
Will <edant.io@proton.me>
2023-09-10 12:58:48 -0400

    nix/flake: use bin, hdr targets

--- flake.nix
+++ flake.nix
@@ -15,37 +15,33 @@
     ( system:
         let
           pkgs = import nixpkgs { inherit system; };
-          # CMake to build; cacert and git for CMake's fetchcontent
-          build_deps = (
+          deps = (
             with pkgs;
+            with darwin.apple_sdk.frameworks;
             [ cacert  # For github.com
               clang   # For building
               cmake   # For building
               git     # For cmake fetchcontent
               python3 # Optional; For snitch testing framework
             ]
-          );
-          # Darwin needs some help to use FSEvents, dispatch, etc.
-          maybe_sys_deps = (
-            with pkgs;
-            lib.optionals stdenv.isDarwin([darwin.apple_sdk.frameworks.CoreServices])
+            # If on Darwin, we need this to use FSEvents, dispatch, etc.
+            ++ lib.optionals stdenv.isDarwin([ CoreServices ])
           );
           watcher = (
-            with pkgs;
-            stdenv.mkDerivation {
-              pname = "watcher";
+            pkgs.stdenv.mkDerivation {
+              pname = "wtr.watcher";
               version = "0.8.8"; # hook: tool/release
               src = self;
-              nativeBuildInputs = build_deps ++ maybe_sys_deps;
+              nativeBuildInputs = deps;
               # build phase should maybe also run unit tests? no...
               # I think we should make a test target to this flake
-              buildPhase = "cmake --build . --target wtr.watcher --config Release -j3";
+              buildPhase = ''
+                cmake --build . --target wtr.watcher --config Release
+                cmake --build . --target wtr.hdr_watcher
+              '';
               installPhase = ''
                 cmake --install . --prefix "$out" --config Release --component bin
                 cmake --install . --prefix "$out" --config Release --component include
-                echo 'checking installation paths...'
-                ls "$out/bin"
-                ls "$out/include/wtr"
               '';
             }
           );
