74a30a69abd06db2a6017d32ea79003f5eb8f3a4
Will <edant.io@proton.me>
2023-09-10 21:32:31 -0400

    tree: un fk

--- _deps/snitch-build/CMakeFiles/snitch-header-only.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "snitch/snitch_all.hpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/snitch-header-only.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
