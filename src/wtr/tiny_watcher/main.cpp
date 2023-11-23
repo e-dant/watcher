e9599127e3a444f0785ee132c54a3f82548a2e49
Will <edant.io@proton.me>
2023-11-22 20:11:23 -0500

    watcher/tiny: doc

--- src/wtr/tiny_watcher/main.cpp
+++ src/wtr/tiny_watcher/main.cpp
@@ -6,20 +6,20 @@ using namespace std;
 using namespace wtr;
 
 // The event type, and every field within it, has
-// string conversions and stream operators. Yes,
-// all kinds of strings, narrow and wide and the
-// odd ones. If we don't want formatting control,
-// we can json-serialize and show the event as:
+// string conversions and stream operators. All
+// kinds of strings, the narrow, wide and odd ones.
+// If we don't want particular formatting, we can
+// json-serialize and show the event like this:
 //   some_stream << event
 // Here, we'll apply our own formatting.
-auto show = [](event e)
+auto show(event e)
 {
   cout << to<string>(e.effect_type) + ' '
         + to<string>(e.path_type)   + ' '
         + to<string>(e.path_name)
         + (e.associated ? " -> " + to<string>(e.associated->path_name) : "")
        << endl;
-};
+}
 
 int main()
 {
