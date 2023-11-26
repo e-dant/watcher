fd6feb1e7542b9368516dc31651709290e49a327
Will <edant.io@proton.me>
2023-11-26 12:08:22 -0500

    watcher/darwin: put back atomics

--- devel/include/detail/wtr/watcher/semabin.hpp
+++ devel/include/detail/wtr/watcher/semabin.hpp
@@ -8,28 +8,17 @@
 #include <string.h>
 #include <sys/eventfd.h>
 #include <unistd.h>
-#elif defined(__APPLE__)
-#include <dispatch/dispatch.h>
 #endif
 
 namespace detail::wtr::watcher {
 
-/*  A semaphore-like construct which can be
-    used with poll and friends on Linux and
-    dispatch on Apple. This class is emulates
-    a binary semaphore; An atomic boolean flag.
-
-    On other platforms, this is an atomic flag
-    which can be checked in a sleep, wake loop,
-    ideally with a generous sleep time.
-
+/*  A semaphore which is pollable on Linux.
+    On all platforms, this behaves like an
+    atomic boolean flag. (On non-Linux, this
+    literally is an atomic boolean flag.)
     On Linux, this is an eventfd in semaphore
-    mode. The file descriptor is exposed for
-    use with poll and friends.
-
-    On macOS, this is a dispatch_semaphore_t,
-    which can be scheduled with dispatch.
-*/
+    mode, which means that it can be waited on
+    with poll() and friends. */
 
 class semabin {
 public:
@@ -39,7 +28,7 @@ private:
   mutable std::atomic<state> is = pending;
 
 public:
-#if defined(__linux__)
+#ifdef __linux__
 
   int const fd = eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK | EFD_SEMAPHORE);
 
@@ -79,53 +68,6 @@ public:
   }
 
   inline ~semabin() noexcept { close(this->fd); }
-
-#elif defined(__APPLE__)
-
-  dispatch_semaphore_t const sema = []()
-  { return dispatch_semaphore_create(0); }();
-
-  inline auto release() noexcept -> state
-  {
-    auto write_ev = [this]()
-    {
-      if (dispatch_semaphore_signal(this->sema) == 1)
-        return released;
-      else
-        return error;
-    };
-
-    if (this->is == released)
-      return released;
-    else
-      return this->is = write_ev();
-  }
-
-  inline auto state() const noexcept -> state
-  {
-    auto read_ev = [this]()
-    {
-      if (dispatch_semaphore_wait(this->sema, DISPATCH_TIME_NOW) == 0)
-        return released;
-      else
-        return pending;
-    };
-
-    if (this->is == pending)
-      return pending;
-    else
-      return this->is = read_ev();
-  }
-
-  inline auto block_on(dispatch_queue_t queue) const noexcept -> bool
-  {
-    dispatch_sync(queue, ^{
-      dispatch_semaphore_wait(this->sema, DISPATCH_TIME_FOREVER);
-    });
-    return this->state() == released;
-  }
-
-  inline ~semabin() noexcept { dispatch_release(this->sema); }
 
 #else
 
