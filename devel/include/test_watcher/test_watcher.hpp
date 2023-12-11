f0d4934d342cd6c2cabf8cf1e65c8b069c403a1f
Will <edant.io@proton.me>
2023-12-10 19:22:49 -0500

    watcher/test: perf, rapid openclose

--- devel/include/test_watcher/test_watcher.hpp
+++ devel/include/test_watcher/test_watcher.hpp
@@ -6,4 +6,5 @@
 #include "test_watcher/event.hpp"
 #include "test_watcher/filesystem.hpp"
 #include "test_watcher/is_verbose.hpp"
+#include "test_watcher/perf.hpp"
 #include "test_watcher/watch_gather.hpp"
