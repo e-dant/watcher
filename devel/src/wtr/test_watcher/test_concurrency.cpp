0bd95e4cee9c33e0e9ef01f0951a5e293a58e62e
Will <edant.io@proton.me>
2023-12-16 16:24:25 -0500

    watcher/test: concurrency at 8 for more reproducible, less noisy tests

--- devel/src/wtr/test_watcher/test_concurrency.cpp
+++ devel/src/wtr/test_watcher/test_concurrency.cpp
@@ -15,7 +15,7 @@ TEST_CASE(
   using namespace wtr::test_watcher;
 
   auto event_count_per_watcher = 10;
-  auto concurrency_level = 16;
+  auto concurrency_level = 8;
 
   check_event_lists_set_eq(watch_gather(
     "Concurrency",
